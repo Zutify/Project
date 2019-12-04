13dabcee6f86179c776499d726b7f0f84c09fc7a
patykpawel <p@tykZgon>
2019-12-04 21:05:18 +0100

    edit: forgot and reset password v1

--- projekt_zespołowy/page/passwordReset.php
+++ projekt_zespołowy/page/passwordReset.php
@@ -1,5 +1,10 @@
 <!-- formularz logowania-->
 <?php
+if(isset($_GET['email']) == true){
+    $email = $_GET['email'];
+    $vkey = $_GET['vkod'];
+}
+
 ?>
 
 <div class="container-fluid" style="min-width: 250px;">
@@ -9,48 +14,54 @@
             Wstecz
         </a>
     </div>
-    
+
     <div class="mt-5 mb-5 ml-5">
         <div class="display-1 text-black">
-            <strong>Zresetuj hasło</strong> 
+            <strong>Zresetuj hasło</strong>
         </div>
     </div>
-            
+
     <div class="w-75 mx-auto ">
-        
-        <form action="php/password_help.php" method="post" >
-            
-            <div class="mt-5 mb-3">
-                <label for="newPassword" class="h3">Nowe hasło</label>
-            </div>
-                    
-            <div >
-                <input type="password" name="newPassword" id="newPassword" class="form-control form-control-lg" style="height: 80px; font-size: 20pt;"<?php check_input('newPassword'); ?>>
-            </div>
-                   
-            <div>
-                <label class="h4 text-danger"><?php check_msg('newPassword'); ?></label>
-            </div>
-                
-            
-            <div class="mt-5 mb-3">
-                <label for="repeatPassword" class="h3 ">Powtórz hasło</label>
-            </div>
-                
-                    
-            <div >
-                <input type="password" name="repeatPassword" id="repeatPassword" class="form-control form-control-lg" style="height: 80px; font-size: 20pt;"<?php check_input('repeatPassword'); ?>>
-            </div>
-                    
-                
-            <div >
-                <label class="h4 text-danger"><?php check_msg('repeatPassword'); ?></label>
-            </div>
-                
-            <div >
-                <input type="submit" value="DALEJ" name="submitReset" class="btn btn-success btn-lg btn-block" style="height: 120px; font-size: 50px; margin-top: 150px;">
-            </div>
-                
+        <?php
+        if(isset($email) && isset($vkey)){
+            echo "<form action='php/password_help.php?vkod=$vkey&email=$email' method='post' >";
+        }else{
+            echo "<form action='php/password_help.php?vkod=$vkey&email=$email' method='post' >";
+        }
+        ?>
+
+
+        <div class="mt-5 mb-3">
+            <label for="newPassword" class="h3">Nowe hasło</label>
+        </div>
+
+        <div >
+            <input type="password" name="newPassword" id="newPassword" class="form-control form-control-lg" style="height: 80px; font-size: 20pt;"<?php check_input('newPassword'); ?>>
+        </div>
+
+        <div>
+            <label class="h4 text-danger"><?php check_msg('newPassword'); ?></label>
+        </div>
+
+
+        <div class="mt-5 mb-3">
+            <label for="repeatPassword" class="h3 ">Powtórz hasło</label>
+        </div>
+
+
+        <div >
+            <input type="password" name="repeatPassword" id="repeatPassword" class="form-control form-control-lg" style="height: 80px; font-size: 20pt;"<?php check_input('repeatPassword'); ?>>
+        </div>
+
+
+        <div >
+            <label class="h4 text-danger"><?php check_msg('repeatPassword'); ?></label>
+        </div>
+
+        <div >
+            <input type="submit" value="DALEJ" name="submitReset" class="btn btn-success btn-lg btn-block" style="height: 120px; font-size: 50px; margin-top: 150px;">
+        </div>
+
         </form>
     </div>
 </div>
--- projekt_zespołowy/php/password_help.php
+++ projekt_zespołowy/php/password_help.php
@@ -4,15 +4,66 @@ include 'db_connection.php';
 # do przypomnienia hasła
 if(isset($_POST['submitForget']))
 {
-    
+    $email = $_POST['email'];
+    $sql = "SELECT * FROM `User` WHERE `Email` LIKE '$email';";
+    $result = $conn->query($sql);
+    if($result->num_rows == 1){
+        $row = $result->fetch_assoc();
+        $vkey = md5(time().$row['Email']);
+        $date = date("Y-m-d H:i:s");
+        $sql = "INSERT INTO ForgotPasswordKey VALUES (NULL , '$row[ID]' ,'$vkey' ,'$date','0');";
+        $conn->query($sql);
+
+        #https://www.tempmailaddress.com/ tutaj tworzy sie 5min maila
+        $reciver = "shloma.jestin@iiron.us";
+        $subject  = "Przypomnienie hasła";
+        $link = "<a href = 'http://zutify.000webhostapp.com/index.php?page=passwordReset&vkod=$vkey&email=$email'>Link</a>";
+        $text = "Aby przypomnieć hasło prosze kliknąć w link : </br>".$link;
+        if(mail($reciver, $subject, $text, $reciver)){
+
+        }
+
+
+        header('Location: ../index.php?page=login');
+    }else{
+        $error = "Blad";
+    }
 }
 
 
 # do resetowania hasła
 if(isset($_POST['submitReset']))
 {
-    
-    
+    if(isset($_GET['email']) == true){
+        $email = $_GET['email'];
+        $vkey = $_GET['vkod'];
+    }
+
+    $sql = "SELECT * FROM `User` WHERE `Email` LIKE '$email';";
+    $result = $conn->query($sql);
+    if($result->num_rows == 1) {
+        $user = $result->fetch_assoc();
+
+    }else{
+        echo "Błąd";
+    }
+    $sql = "SELECT * FROM ForgotPasswordKey WHERE `VerificationKey` LIKE '$vkey' AND `WasUsed` = '0' AND `UserID` = '$user[ID]'
+            LIMIT 1;";
+    $result = $conn->query($sql);
+
+    if($result->num_rows == 1){
+        $sql = "UPDATE ForgotPasswordKey SET `WasUsed` = 1 WHERE `VerificationKey` LIKE '$vkey' AND `WasUsed` = '0' AND `UserID` = '$user[ID]';";
+        $conn->query($sql);
+
+        $password = md5($_POST["newPassword"]);
+        $sql = "UPDATE User SET `Password` = '$password' WHERE `ID` = '$user[ID]';";
+        $conn->query($sql);
+    }else{
+        echo "Blad";
+    }
+
+
+
 }
 
 ?>
