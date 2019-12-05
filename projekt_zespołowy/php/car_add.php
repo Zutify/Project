8b5250d5130b3c06a63a8d0da787e0cbc4371bf7
baldorsz <bartosz.gryfice@gmail.com>
2019-12-06 00:26:51 +0100

    added php script to create user car in data base

---
+++ projekt_zespołowy/php/car_add.php
@@ -0,0 +1,34 @@
+<?php
+include 'db_connection.php';
+session_start();
+include 'user_data.php';
+
+if(isset($_POST["submit"]))
+{
+    if(!empty($_POST['carBrand']) and !empty($_POST['carModel']) and !empty($_POST['phoneNumber']) and !empty($_POST['driverDescription']))
+    {
+        $car_barnd = $_POST['carBrand'];
+        $car_model = $_POST['carModel'];
+        $phone_number = $_POST['phoneNumber'];
+        $driver_desc = $_POST['driverDescription'];
+        $sql = "INSERT into UserCar VALUES ('$id', '$car_barnd', '$car_model', '$driver_desc')";
+        $result_car = $conn->query($sql);
+        if(!$result_car)
+        {
+            $_SESSION['carError'] = "Błąd przy dodawaniu przejazdu";
+            header('Location: ../index.php?page=driverInfo');
+            exit();
+        }
+        else
+        {
+            // kasowanie zmiennych sesyjnych związanych z dodaniem przejazdu
+            if(isset($_SESSION['carError']))
+            {
+                unset($_SESSION['carError']);
+            }
+            header('Location: ../index.php?page=menu');
+            exit();
+        }
+    }
+}
+?>
