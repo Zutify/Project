d7c7973289f0d2dd0eef08eccce7d030132e269e
mati3457 <mati3457@gmail.com>
2019-12-07 23:54:26 +0100

    dim site when sideMenu open

--- projekt_zespołowy/page/sideMenu.php
+++ projekt_zespołowy/page/sideMenu.php
@@ -80,17 +80,18 @@ include '../php/reset_ride_search.php';
     $(document).ready(function () {
         $('#side-menu-noclick').click(function(){
             document.getElementById('btn-side-menu').style.display = "block";
-            $.when($('#sideMenu').fadeOut("slow")).done(function() {
-                 $('#sideMenu *').detach()
-                 $('#side-menu-noclick').detach()
-                 });
+            $.when($('#sideMenu, #side-menu-noclick').fadeOut("slow")).done(function() {
+            $('#sideMenu *').detach()
+            $('#side-menu-noclick').detach()
+            });
         });
+
         $('#btn-side-menuClose').click(function(){
             document.getElementById('btn-side-menu').style.display = "block";
-            $.when($('#sideMenu').fadeOut("slow")).done(function() {
-                 $('#sideMenu *').detach()
-                 $('#side-menu-noclick').detach()
-                 });
+            $.when($('#sideMenu, #side-menu-noclick').fadeOut("slow")).done(function() {
+            $('#sideMenu *').detach()
+            $('#side-menu-noclick').detach()
+            });
         });
     });
 </script>
