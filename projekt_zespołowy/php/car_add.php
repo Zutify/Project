76409e4d13eaaf847f319ae171a7cfef67d3da8a
baldorsz <bartosz.gryfice@gmail.com>
2019-12-12 11:04:52 +0100

    update car_add.php

--- projekt_zespołowy/php/car_add.php
+++ projekt_zespołowy/php/car_add.php
@@ -5,37 +5,70 @@ include 'user_data.php';
 
 if(isset($_POST["submit"]))
 {
-    if(!empty($_POST['carBrand']) and !empty($_POST['carModel']) and !empty($_POST['phoneNumber']) and !empty($_POST['driverDescription']))
+    
+    if(!empty($_POST['carBrand']) and !empty($_POST['carModel']) and !empty($_POST['driverDescription']))
     {
-        $car_barnd = $_POST['carBrand'];
+        $car_brand = $_POST['carBrand'];
         $car_model = $_POST['carModel'];
-        $phone_number = $_POST['phoneNumber'];
         $driver_desc = $_POST['driverDescription'];
-        $sql = "INSERT into UserCar VALUES ('$id', '$car_barnd', '$car_model', '$driver_desc')";
-        $result_car = $conn->query($sql);
-        if(!$result_car)
+        
+        //$userID = intval($id);
+        // sprawdzenie czy rekord dla danego użytkownika już istnieje
+        $sql = "Select * from UserCar where UserID = $id";
+        if($result_car = $conn->query($sql))
         {
-            $_SESSION['carError'] = "Błąd przy dodawaniu przejazdu";
-            header('Location: ../index.php?page=driverInfo');
-            exit();
+            // jeśli istnieje to update
+            $sql2 = "Update UserCar SET `Brand` = '$car_brand', `Model` = '$car_model', `Description` = '$driver_desc' where UserID = $id";
+            
+            $result_car = $conn->query($sql2);
+            if(!$result_car)
+            {
+                $_SESSION['carError'] = "Błąd przy aktualizowaniu danych kierowcy";
+                header('Location: ../index.php?page=driverInfo');
+                exit();
+            }
+            else
+            {
+                // kasowanie zmiennych sesyjnych związanych z dodaniem przejazdu
+                if(isset($_SESSION['carError']))
+                {
+                    unset($_SESSION['carError']);
+                }
+                header('Location: ../index.php?page=menu');
+                exit();
+    
+            }
         }
         else
         {
-            // kasowanie zmiennych sesyjnych związanych z dodaniem przejazdu
-            if(isset($_SESSION['carError']))
+            // jeśli nie istnieje to wstawić
+            $sql2 = "INSERT into UserCar VALUES ($id ,'$car_brand', '$car_model', '$driver_desc')";
+            
+            $result_car = $conn->query($sql2);
+            if(!$result_car)
             {
-                unset($_SESSION['carError']);
+                $_SESSION['carError'] = "Błąd przy dodawaniu danych kierowcy";
+                header('Location: ../index.php?page=driverInfo');
+                exit();
             }
-            header('Location: ../index.php?page=menu');
-            exit();
-
+            else
+            {
+                // kasowanie zmiennych sesyjnych związanych z dodaniem przejazdu
+                if(isset($_SESSION['carError']))
+                {
+                    unset($_SESSION['carError']);
+                }
+                header('Location: ../index.php?page=menu');
+                exit();
+    
+            }
         }
     }
     else
     { 
-
         header('Location: ../index.php?page=editProfile');
         exit();
     } 
+    
 }
 ?>
